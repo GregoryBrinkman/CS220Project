1d848d00758a4324f3b960bb7848bf7d70e05024
root <GregoryLucasBrinkman@gmail.com>
2015-12-27 05:32:29 -0600

    started commenting

--- Elevator.java
+++ Elevator.java
@@ -1,40 +1,40 @@
 class Elevator
 {
-	private LinkedStack<Integer> currentFloor;
+	private LinkedStack<Integer> currentFloor; //allocate memory for LinkedStack, currentFloor
 
-	public Elevator()
+	public Elevator() //default constructor
 	{
-		currentFloor = new LinkedStack<Integer>();
-		currentFloor.push(1);
-	}
+		currentFloor = new LinkedStack<Integer>(); //initialize currentFloor
+		currentFloor.push(1); //initialize currentFloor to the first floor
+	}//end constructor
 
 	public void goUp()
 	{
-		currentFloor.push(currentFloor.peek()+1);
-		waitOne();
-	}
+		currentFloor.push(currentFloor.peek()+1); //pushes currentFloor plus one
+		waitOne(); //forces system to wait, making the elevator more realistic
+	}//end goUp
 
 	public void goDown()
 	{
-		currentFloor.pop();
-		waitOne();
-	}
+		currentFloor.pop(); //pops the last pushed floor number
+		waitOne(); //forces system to wait, making the elevator more realistic
+	}//end goDown
 
 	public int getFloor()
 	{
-		return currentFloor.peek();
-	}
+		return currentFloor.peek(); //currentFloor will be the first integer in the stack
+	}//end getFloor
 
 	public void goToFloor(int f)
 	{
-		while(getFloor() != f)
+		while(getFloor() != f) //compares currentFloor to the desired floor
 		{
-			if(getFloor() > f)
+			if(getFloor() > f) //if the currentfloor is greater 
 				goDown();
 			else
 				goUp();
-		}
-	}
+		}//end while
+	}//end goToFloor
 
 	public int floorAt(int floor)
 	{
